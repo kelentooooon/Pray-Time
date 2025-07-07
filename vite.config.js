6208914c3712520a55ef3249701164f63fd91ae2
kelentooooon <ehapm5678@gmail.com>
2025-07-07 21:41:09 +0300

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import withMT from "@material-tailwind/react/utils/withMT"
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 export default defineConfig(
   withMT({
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+    plugins: [react(), tailwindcss()],
+    build: {
+      cssCodeSplit: false,
+    },
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
       extend: {},
     },
   })
-)
+);
